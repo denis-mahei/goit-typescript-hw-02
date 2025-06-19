88852c9304a746d7cbffba7d1a7919d22442104c
denis-mahei <denmahei@gmail.com>
2025-06-19 19:23:56 +0300

    fix vite -v

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
+import { fileURLToPath } from 'node:url';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { fileURLToPath } from 'node:url';
 
 // https://vite.dev/config/
 const __filename = fileURLToPath(import.meta.url);
